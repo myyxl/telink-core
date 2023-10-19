0492b2682a5d53e3e0e1f435bbfb7e4d41fc698a
myyxl <22593897+myyxl@users.noreply.github.com>
2023-10-19 10:58:22 +0200

    feat(pcap): receive packet

--- src/pcap/receiver.rs
+++ src/pcap/receiver.rs
@@ -1,12 +1,14 @@
 use std::sync::{Arc, Mutex};
 use std::thread;
-use std::time::{Duration, SystemTime};
-use pcap::{Device, Capture, Linktype};
+use std::time::SystemTime;
+use pcap::{Capture, Linktype};
 
 use crate::State;
 
 pub fn start_packet_capture(state: Arc<Mutex<State>>) {
     thread::spawn(move || {
+        let id = &generate_identifier("telink-core").to_be_bytes()[4..7];
+
         let mut capture = Capture::from_device("wlp4s0mon")
             .unwrap()
             .immediate_mode(true)
@@ -16,14 +18,45 @@ pub fn start_packet_capture(state: Arc<Mutex<State>>) {
         capture.set_datalink(Linktype::IEEE802_11_RADIOTAP).unwrap();
 
         while let Ok(packet) = capture.next_packet() {
+
+            // Check packet type
+            if  (packet.data[56] as usize) != 8 {
+                continue;
+            }
+
+            // Check if core is the receiver
+            let receiver = &packet.data[66..69];
+            if receiver != id {
+                continue;
+            }
+
+            // Read data
+            let size: [u8; 2] = (&packet.data[80..82]).try_into().unwrap();
+            let size: u16 = u16::from_be_bytes(size).into();
+            let size: usize = size.into();
+            let data = &packet.data[82 .. 82 + size - 1];
+
+            // Convert to &str
+            let message = String::from_utf8_lossy(data);
+            let message = message.trim();
+
+            // Write into queue
             match state.lock() {
                 Ok(mut lock) => {
-                    lock.queue.push_back(String::from(format!("{:?}", packet.header)));
+                    lock.queue.push_back(String::from(message));
                     lock.controller_last_ping = Some(SystemTime::now());
                 }
                 Err(_) => ()
             }
         }
-        println!("After")
     });
+}
+
+fn generate_identifier(name: &str) -> u64 {
+    let mut hash: u64 = 5381;
+    for c in name.chars() {
+        let c: u64 = c.into();
+        hash = ((hash << 5) + hash) + c;
+    }
+    hash & 0xFFFFFFFF
 }
